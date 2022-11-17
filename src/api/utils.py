398009608306d8abd59f68c3470ee46cb2d5e285
Safat99 <sarkersafat99@gmail.com>
2022-11-17 10:25:11 +0600

    unique user id for each role created

---
+++ src/api/utils.py
@@ -0,0 +1,31 @@
+from src.models import User
+import string
+import random
+
+
+def random_string(digit):
+    chars = string.ascii_lowercase + string.digits
+    random_string = "".join(random.choice(chars) for _ in range(digit))
+    return random_string
+
+
+def unique_user_id(role):
+    roles = {
+        'guest' : 'G',
+        'faculty' : 'F',
+        'student' : 'S',
+        'guardian': 'P'
+    }
+
+    if (roles.get(role,"invalid_role") == 'invalid_role'):
+        return 'error'
+
+    user = User.query.filter_by(uid=uid).first().uid
+    while user == uid:
+        uid = roles.get(role) + random_string(5)
+    return uid
+
+    
+
+
+
