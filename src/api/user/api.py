3a1989c2a38c7303507fc24cc681e7ade3c87258
Safat99 <sarkersafat99@gmail.com>
2022-11-18 14:09:47 +0600

    temp change for checking remote origin

--- src/api/user/api.py
+++ src/api/user/api.py
@@ -4,10 +4,12 @@ from flask_restx import Resource
 from src.models import User, SuperUser
 from src.api import utils
 
-class RegisterSuperUser(Resource):
+class RegisterSuwwwwwwwperUser(Resource):
     def post(self):
         """Register new admin"""
         payload = request.get_json()
+        name = payload.get("name")
         email = payload.get("email")
         password = payload.get("password")
+        resp = {}
         
