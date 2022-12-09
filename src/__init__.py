4120b086eeae83b8c58cf6e21753f438b50177c3
Safat99 <sarkersafat99@gmail.com>
2022-12-10 05:16:22 +0600

    cors error fixed

--- src/__init__.py
+++ src/__init__.py
@@ -3,22 +3,25 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 
+cors = CORS()
 db = SQLAlchemy()
 migrate = Migrate()
 bcrypt = Bcrypt()
-from src import models
 
 def create_app():
     app = Flask(__name__)
     app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
+    cors.__init__(app)
     db.init_app(app)
-    migrate.init_app(app,db)
     bcrypt.init_app(app)
 
+    from src import models
+    migrate.init_app(app,db)
     from src.api import api
     api.init_app(app)
 
